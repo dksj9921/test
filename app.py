a4a0cd41a697efd88ff29f63c2ed82e619749ec7
dksj9921 <83447917+dksj9921@users.noreply.github.com>
2023-01-27 16:44:31 +0900

    커밋

--- app.py
+++ app.py
@@ -1,22 +1,43 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-st.title("Avocado Prices dashboard")
+import tensorflow as tf
+from keras.applications.vgg16 import VGG16
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.utils import img_to_array
+from tensorflow.image import pad_to_bounding_box
+from tensorflow.image import central_crop
+from tensorflow.image import resize
+
+
+st.title("Image Classification")
 st.markdown('''
-테스트용 데시보드를 만들어봅시다.
-Data source: [kaggle](https://www.kaggle.com/datasets/timmate/avocado-prices-2020)
+
+
 ''')
-avocado = pd.read_csv("avocado.csv")
-table = avocado.groupby("type")[['total_volume','average_price']].mean()
-st.dataframe(table)
-selected_geo = st.selectbox(label="Geography",options=avocado['geography'].unique())
-submitted = st.button("Submit")
-if submitted:
-    line_fig = px.line(
-        avocado[avocado['geography']==selected_geo],
-        x='date',y='average_price',
-        color='type',
-        title=f'{selected_geo} dashboard'
-    )
-    st.plotly_chart(line_fig)
+file = st.file_uploader('이미지를 올려주세요.', type=['jpg','png'])
+
+if file is None : 
+    st.markdown('''### 이미지를 먼저 올려주세요.''')
+else :
+    bgd = file
+    bgd_vector = np.asarray(img_to_array(bgd), dtype = None)
+    bgd_vector = bgd_vector/255
  
+    #이미지 형태 확인 
+    bgd_vector.shape
+ 
+    #이미지 확인 
+    plt.imshow(bgd_vector)
+    st.plt.show()
+
+
+print(tf.version)
+ 
+ 
+#weight, include_top 파라미터 설정 
+model = VGG16(weights='imagenet', include_top=True)
+
+
+
