f6ee0bb6cc922b7dd4beadaddaee99f9730c0eb3
SungHwan <sung980213@naver.com>
2023-01-30 01:24:34 +0900

    프로젝트 완성

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ import plotly.express as px
 import tensorflow as tf
 from keras.applications.vgg16 import VGG16
 import numpy as np
-import matplotlib.pyplot as plt
 from keras.utils import img_to_array
 from PIL import Image
-# from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing import image
-import matplotlib.pyplot as plt
 from PIL import Image
 from keras.applications.imagenet_utils import decode_predictions
 from keras.applications.imagenet_utils import preprocess_input
@@ -33,44 +30,42 @@ else :
     bgd_vector = bgd_vector/255
     # print(bgd_vector)
  
-    #이미지의 변경할 크기 설정 
-    target_height = 4500
-    target_width = 4500
+    w, h = bgd.size
+    s = min(w, h)
+    y = (h - s) // 2
+    x = (w - s) // 2
     
-    #현재 이미지의 크기 지정 
-    source_height = bgd_vector.shape[0]
-    source_width = bgd_vector.shape[1]
-    
-    #padding 실시 : pad_to_bounding_box 사용 
-    bgd_vector_pad = tf.image.pad_to_bounding_box(bgd_vector, 
-                                        int((target_height-source_height)/2), 
-                                        int((target_width-source_width)/2), 
-                                        target_height, 
-                                        target_width)
-                                        
+    print(w, h, x, y, s)
+    bgd_vector_pad = bgd.crop((x, y, x+s, y+s))
+    # 4-tuple defining the left, upper, right, and lower pixel coordinate
+
+                               
     #이미지 형태 확인 
-    bgd_vector_pad.shape
 
     model = VGG16(weights='imagenet', include_top=True)
 
     target_size = 224
     img = tf.image.resize(bgd_vector_pad, (target_size, target_size))
     
-    np_img = img_to_array(img)
- 
- 
-    # #4차원으로 변경 
-    img_batch = np.expand_dims(np_img, axis=0)
+    img_batch = preprocess_input(img)
 
-    img_batch = preprocess_input(img_batch)
+    img_batch = img_to_array(img)
+
+    pre_processed = np.expand_dims(img_batch, axis=0)
+
+   
+    y_preds = model.predict(pre_processed)
+    np.set_printoptions(suppress=True, precision=10)
+    y_preds.shape
+    
+    y_preds = decode_predictions(y_preds, top=11)
+
+    y_preds
     
 
 
 
 
- 
- 
-#weight, include_top 파라미터 설정 
 
 
 
